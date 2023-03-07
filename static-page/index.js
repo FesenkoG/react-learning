9f967cd93f281a21394ffe1e3072029f1642b373
Георгий Фесенко <georgii@Georgijs-Air.lan>
2023-03-07 14:48:21 -0500

    First commit

---
+++ static-page/index.js
@@ -0,0 +1,12 @@
+// const h1 = document.createElement("h1")
+// h1.textContent = "Hello world"
+// h1.className = "header"
+// console.log(h1)
+
+const page = (
+    <div>
+        <h1 className="header">This is JSX</h1>
+        <p>This is a paragraph</p>
+    </div>
+)
+ReactDOM.render(page, document.getElementById("root"))
