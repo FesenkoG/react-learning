c17a69c1eaacf2d85a2e6428e0508511cd909e91
Georgii <georgii@percents.com>
2023-03-15 12:29:06 -0400

    Done

---
+++ css-grid/src/App.js
@@ -0,0 +1,168 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <main className="grid">
+      <article className="featured-article">
+        <div className="featured-article-text">
+          <h2><span className="live"> LIVE</span>Royal family to open new children's hospital</h2>
+          <p>
+            The royal family to open up a new hospital dedicated to helping children across the UK using specialist equipment.
+          </p>
+          <div className="meta-info">
+            <span className="category">UK</span>
+          </div>
+        </div>
+
+        <div className="featured-article-img">
+          <img src="https://ichef.bbci.co.uk/news/820/cpsprodpb/1048/production/_128986140_gettyimages-1439274747.jpg" alt="hospital entrance" />
+        </div>
+
+      </article>
+      <article className="second-row">
+        <ul>
+          <li>Who are the royal family?</li>
+        </ul>
+      </article>
+      
+
+      <article className="regular-article">
+        <img src="https://ichef.bbci.co.uk/news/240/cpsprodpb/12761/production/_128971657_swb-index-getty.jpg" alt="train at station" />
+          <h3>New highspeed railway announced</h3>
+          <p>
+            A new railway linking London to Portsmouth is under construction, due to be completed in 2074.
+          </p>
+          <div className="meta-info">
+            <img className="icon" src="assets/clock-regular.svg" />
+            <span className="time">1h </span>
+            <span className="divider">|</span>
+            <span className="category"> Travel </span>
+          </div>
+      </article>
+
+      <article className="regular-article">
+        <img src="https://ichef.bbci.co.uk/news/240/cpsprodpb/12761/production/_128971657_swb-index-getty.jpg" alt="two rockets landing" />
+        <h3>SpaceX lands two rockets at once</h3>
+        <p>
+          SpaceX has completed the feat of landing two rockets side-by-side simultaneously.
+        </p>
+        <div className="meta-info">
+          <img className="icon" src="assets/clock-regular.svg" />
+          <span className="time">4h </span>
+          <span className="divider">|</span>
+          <span className="category"> Science </span>
+        </div>
+      </article>
+
+      <article className="regular-article">
+        <img src="https://ichef.bbci.co.uk/news/240/cpsprodpb/12761/production/_128971657_swb-index-getty.jpg" alt="police" />
+        <h3>Police stopped demonstration in London</h3>
+        <p>
+          A demonstration has been halted by police after riots broke out in central London.
+        </p>
+        <div className="meta-info">
+          <img className="icon" src="assets/clock-regular.svg" />
+          <span className="time">9h </span>
+          <span className="divider">|</span>
+          <span className="category"> UK </span>
+        </div>
+      </article>
+
+      <article className="regular-article">
+        <img src="https://ichef.bbci.co.uk/news/240/cpsprodpb/12761/production/_128971657_swb-index-getty.jpg" alt="sports cyclists on a road" />
+        <h3>Tour de France - the story of this year's race</h3>
+        <p>
+          A stage-by-stage guide to this year's dramatic Tour de France, including commentary from experts.
+        </p>
+        <div className="meta-info">
+          <img className="icon" src="assets/clock-regular.svg" />
+          <span className="time">8h </span>
+          <span className="divider">|</span>
+          <span className="category"> Sports </span>
+        </div>
+      </article>
+
+      <article className="regular-article">
+        <img src="https://ichef.bbci.co.uk/news/240/cpsprodpb/12761/production/_128971657_swb-index-getty.jpg" alt="eggs in a box" />
+        <h3>Eggs and margarine drive food inflation over 17%</h3>
+        <p>
+          Rising prices put more pressure on households, with one in four shoppers struggling, research finds.
+        </p>
+        <div className="meta-info">
+          <img className="icon" src="assets/clock-regular.svg" />
+          <span className="time">1h </span>
+          <span className="divider">|</span>
+          <span className="category"> Business </span>
+        </div>
+      </article>
+      <article className="regular-article">
+        <img src="https://ichef.bbci.co.uk/news/240/cpsprodpb/12761/production/_128971657_swb-index-getty.jpg" alt="eggs in a box" />
+        <h3>Eggs and margarine drive food inflation over 17%</h3>
+        <p>
+          Rising prices put more pressure on households, with one in four shoppers struggling, research finds.
+        </p>
+        <div className="meta-info">
+          <img className="icon" src="assets/clock-regular.svg" />
+          <span className="time">1h </span>
+          <span className="divider">|</span>
+          <span className="category"> Business </span>
+        </div>
+      </article>
+      <article className="regular-article">
+        <img src="https://ichef.bbci.co.uk/news/240/cpsprodpb/12761/production/_128971657_swb-index-getty.jpg" alt="eggs in a box" />
+        <h3>Eggs and margarine drive food inflation over 17%</h3>
+        <p>
+          Rising prices put more pressure on households, with one in four shoppers struggling, research finds.
+        </p>
+        <div className="meta-info">
+          <img className="icon" src="assets/clock-regular.svg" />
+          <span className="time">1h </span>
+          <span className="divider">|</span>
+          <span className="category"> Business </span>
+        </div>
+      </article>
+      <article className="regular-article">
+        <img src="https://ichef.bbci.co.uk/news/240/cpsprodpb/12761/production/_128971657_swb-index-getty.jpg" alt="eggs in a box" />
+        <h3>Eggs and margarine drive food inflation over 17%</h3>
+        <p>
+          Rising prices put more pressure on households, with one in four shoppers struggling, research finds.
+        </p>
+        <div className="meta-info">
+          <img className="icon" src="assets/clock-regular.svg" />
+          <span className="time">1h </span>
+          <span className="divider">|</span>
+          <span className="category"> Business </span>
+        </div>
+      </article>
+      <article className="regular-article">
+        <img src="https://ichef.bbci.co.uk/news/240/cpsprodpb/12761/production/_128971657_swb-index-getty.jpg" alt="eggs in a box" />
+        <h3>Eggs and margarine drive food inflation over 17%</h3>
+        <p>
+          Rising prices put more pressure on households, with one in four shoppers struggling, research finds.
+        </p>
+        <div className="meta-info">
+          <img className="icon" src="assets/clock-regular.svg" />
+          <span className="time">1h </span>
+          <span className="divider">|</span>
+          <span className="category"> Business </span>
+        </div>
+      </article>
+      <article className="regular-article">
+        <img src="https://ichef.bbci.co.uk/news/240/cpsprodpb/12761/production/_128971657_swb-index-getty.jpg" alt="eggs in a box" />
+        <h3>Eggs and margarine drive food inflation over 17%</h3>
+        <p>
+          Rising prices put more pressure on households, with one in four shoppers struggling, research finds.
+        </p>
+        <div className="meta-info">
+          <img className="icon" src="assets/clock-regular.svg" />
+          <span className="time">1h </span>
+          <span className="divider">|</span>
+          <span className="category"> Business </span>
+        </div>
+      </article>
+    </main>
+  );
+}
+
+export default App;
